161f6abf712e5ab62973975f459eae77fbb9239a
Peter Tao <ptao@umass.edu>
2018-02-10 02:37:46 +0000

    added print

--- examples/button_example.py
+++ examples/button_example.py
@@ -1,7 +1,7 @@
 from gpiozero import Button
 from signal import pause
 
-
+print("Press ctrl+c to quit at any time")
 pin = input("Which GPIO pin is the button? ")
 
 button = Button(int(pin))
--- examples/led_example.py
+++ examples/led_example.py
@@ -1,6 +1,8 @@
 from gpiozero import LED
 from signal import pause
 
+print("Press ctrl+c to quit at any time")
+
 pin = input("Which GPIO pin is the LED? ")
 
 led = LED(int(pin))
